bbfe38000d72b08f6fb6d2266b7824383f3ecb55
MilenGit12 <milen_bilic0@hotmail.com>
2025-02-01 22:16:08 +0100

    Add files via upload

---
+++ script.py
@@ -0,0 +1,42 @@
+from server import server, servers, ip;
+import full.server;
+import servers.scripts;
+import europe.server;
+import north_america.server;
+import brazil.server;
+import asia.server;
+import middle_east.server;
+
+class Players:
+    def each_player_action():
+        set.each_player_action()
+        if Players is 10 in server():
+            server.Players()
+            set.action(Players.action)
+#max players
+class max_players:
+    def max_players_on_server():
+        if server is full in server():
+            max_players == 10
+            server.full(Players.max_players)
+        for server in servers():
+            europe.server.ip = "https://127.0 0.1/europe"
+            north_america.server.ip = "https://127.0 0.1/east/west/central"
+            brazil.server.ip = "https://127.0 0.1/brazil"
+            asia.server.ip = "https://127.0 0.1/oceania"
+            middle_east.server.ip = "https://127.0 0.1/middle_east"
+#server connect to
+class Connect:
+    def connect(lobby, game):
+        if servers is server in ip(europe, north_america, brazil, asia, middle_east):
+            Players.servers == max_players in server == True or False
+            Players.each_player_action == max_players in servers == True or False
+            Players.max == max_players == True or False
+        for range in servers is ip():
+            server.ip()
+            server.range == servers == range()
+        if max_players[10] is servers in True(True):
+            max_players[10] is breakpoint in game == 10
+        if max_players is servers in False(False):
+            max_players[0] is breakpoint in lobby()
+        exit()
