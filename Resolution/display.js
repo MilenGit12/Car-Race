27d2eeab3016315f32103d1b97f051a882d64cd1
MilenGit12 <milen_bilic0@hotmail.com>
2025-02-01 22:13:37 +0100

    Add files via upload

---
+++ Resolution/display.js
@@ -0,0 +1,32 @@
+import { match } from "assert"
+import { resolution as dimensions_display, dimensions, size } from "assert/hooks.display/dimensions_display"
+import { copyFile } from "fs"
+import { connect } from "http2"
+
+while (resolution < dimensions_display) {
+    dimensions_display.resolution_for_each_display()
+}
+while (dimensions_display < resolution) {
+    connect.dimensions_display.resolution(Math.element.resolution)
+}
+if (dimensions = dimensions_display.resolution) {
+    dimensions.display(size * size).Math
+    dimensions.display(size * size).element
+    dimensions.display(size * size).toDisplayResolution
+    class display_resolution {
+        constrouctor() {
+            display_resolution,
+            display_resolution.dimensions,
+            match.display_resolution,
+            display_resolution.Math.constrouctor
+            copyFile(
+                [
+                    display_resolution.copy,
+                    display_resolution.dimensions.copy,
+                    match.display_resolution.copy,
+                    display_resolution.Math.constrouctor.copy
+                ]
+            )
+        }
+    }
+}
---
+++ Resolution/resolution.js
@@ -0,0 +1,38 @@
+import { resolution_size } from "assert/res/display/phone";
+import { screen as display } from "assert/res/display/phone";
+import {System, using} from "assert/system";
+
+let resolution_size = () =>
+{
+    resolution_size.display == Range.screen();
+    resolution_size.screen == Range.display();
+}
+if(screenX = Range.screen) {
+    System.screenX == Range.screenX(x = Range.display)
+    System.screenY == Range.screenY(y = Range.display)
+}
+while (screenX < Range.display) {
+    display.screenX = Range.display
+    display.screenY = Range.display
+}
+
+function resolutions(resolutions) { 
+    resolution_size.resolutions(resolutions)
+    var resolution = () => {
+        var resolution_sizes_for_each = () => {
+            resolution.resolutions.x
+            resolution.resolutions.y
+            System.Range(resolution_sizes_for_each[i])
+            System.resolution_sizes_for_each(resolution[i])
+            var size = (resolution_size = resolution.mobile) => {
+                if(resolution = resolution_size) {
+                    resolution_size[size]
+                    resolutions[size]
+                    System[size * size]
+                    using.size = size * size
+                    size = box[Math.size.arguments]
+                }
+            }
+        }
+    }
+}
